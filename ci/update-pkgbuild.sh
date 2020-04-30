f14380866eeaf77d6df1b71e78e4a5682dddb667
Adrian Zankich <azankich@pivotal.io>
2020-04-30 22:04:43 +0000

    Add pipeline to update tools

---
+++ ci/update-pkgbuild.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -exu
+
+apt-get update && apt-get install git
+
+root_dir=$PWD
+
+for tool in "kapp" "ytt" "vendir" "kbld"
+do
+  pushd "${tool}-release"
+    sha=($(sha256sum ${tool}-linux-amd64))
+    version=$(cat version)
+    sed -i "3s/.*/pkgver=${version}/" "${root_dir}/k14s-aur-git/${tool}-bin/PKGBUILD"
+    sed -i "10s/.*/sha256sums=(\"${sha}\")/" "${root_dir}/k14s-aur-git/${tool}-bin/PKGBUILD"
+  popd
+done
+
+pushd "k14s-aur-git/"
+  status="$(git status --porcelain)"
+  if [[ -n "$status" ]]; then
+    git config user.name "ci bot"
+    git config user.email "bot@example.com"
+
+    git add --all .
+    git commit -m "update tools"
+  fi
+popd
+
+shopt -s dotglob
+cp -R "k14s-aur-git/." "updated-k14s-aur-git/"
